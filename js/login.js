d8ec833a413ca6ef704306c6a1267f59454979ee
Guilherme Oliveira <guideoliveira2197@gmail.com>
2023-11-29 21:13:07 -0300

    commit

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ const handleSubmit = (event) => {
   event.preventDefault();
 
   localStorage.setItem('player', input.value);
-  window.location = '/pages/game.html';
+  window.location = '/pages/game/index2.html';
 };
 
 input.addEventListener('input', validateInput);
